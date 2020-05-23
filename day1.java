080e81fde85aa0fdb73a3123a889555f57c2b7d6
EKTA KUMARI <57098554+EKTA-30@users.noreply.github.com>
2020-05-23 10:02:59 +0530

    A bit nervous.

--- day1.java
+++ day1.java
@@ -4,4 +4,5 @@ public static void main(String args[])throws IOException
 {
 Scanner sc=new Scanner(System.in);
 System.out.print("MY FIRST DAY AT GIT");
+System.out.print("MY FIRST DAY AT GIT, EXCITED!!!!");
 }}
