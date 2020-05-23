5614557a58840331605b38a35ab235793d6b1a4f
EKTA KUMARI <57098554+EKTA-30@users.noreply.github.com>
2020-05-23 13:12:44 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -4,5 +4,7 @@ public static void main(String args[])throws IOException
 {
 Scanner sc=new Scanner(System.in);
 System.out.print("MY FIRST DAY AT GIT");
+  
+System.out.print("MY FIRST DAY AT GIT,EAGER");
 System.out.print("MY FIRST DAY AT GIT, EXCITED!!!!");
 }}
